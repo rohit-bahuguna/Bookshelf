99cc3092148cd2d6fc7ce8445a378ec496276f31
rohit-bahuguna <rohitbahuguna.4446.com>
2023-06-18 13:04:26 +0530

    new books added

--- src/utils/booksData.js
+++ src/utils/booksData.js
@@ -6,21 +6,21 @@ export const bookStoreData = [
             {
                 id: 11,
                 title: "The Psychology of Money",
-                author: "Morgan Housel",
-                image: "https://m.media-amazon.com/images/I/41r6F2LRf8L._SX323_BO1,204,203,200_.jpg",
+                author: "AiR Atman in Ravi",
+                image: "https://m.media-amazon.com/images/I/41kA4c9GYIL._SX322_BO1,204,203,200_.jpg",
 
             },
             {
                 id: 12,
-                title: "The Psychology of Money",
+                title: "Happiness is Success",
                 author: "Morgan Housel",
                 image: "https://m.media-amazon.com/images/I/41r6F2LRf8L._SX323_BO1,204,203,200_.jpg"
             },
             {
                 id: 13,
-                title: "The Psychology of Money",
-                author: "Morgan Housel",
-                image: "https://m.media-amazon.com/images/I/41r6F2LRf8L._SX323_BO1,204,203,200_.jpg"
+                title: "Rich Dad Poor Dad",
+                author: " Robert T. Kiyosaki",
+                image: "https://m.media-amazon.com/images/I/41sjdgeKJrL._SX324_BO1,204,203,200_.jpg"
             }
         ]
     },
@@ -29,10 +29,16 @@ export const bookStoreData = [
         category: "Want to Read",
         books: [
             {
-                id: 12,
+                id: 21,
                 title: "Power of your Subconscious Mind",
                 author: "Dr Joseph Murphy",
                 image: "https://m.media-amazon.com/images/I/41WX6XCA5QL._SX308_BO1,204,203,200_.jpg"
+            },
+            {
+                id: 22,
+                title: "The Intelligent Investor",
+                author: "Benjamin Graham ",
+                image: "https://m.media-amazon.com/images/I/91+t0Di07FL._AC_UY218_.jpg"
             }
         ]
     },
@@ -41,10 +47,16 @@ export const bookStoreData = [
         category: "Read",
         books: [
             {
-                id: 13,
+                id: 31,
                 title: "How to Win Friends and Influence People",
                 author: "Dale Carnegie ",
                 image: "https://m.media-amazon.com/images/I/51ay1jgMBNL._SX302_BO1,204,203,200_.jpg"
+            },
+            {
+                id: 32,
+                title: "Doglapan",
+                author: "Ashneer Grover",
+                image: "https://m.media-amazon.com/images/I/81InFh0R4UL._AC_UY218_.jpg"
             }
         ]
     }
